dda2f5f88bf780f4a857ee9616093a0028c66617
João Pedro Borba Pereira <joaopborba95@hotmail.com>
2024-11-01 08:49:15 -0300

    exercitando

---
+++ exerc_operacoes/app.js
@@ -0,0 +1,9 @@
+const num1 = parseFloat(prompt("Digite o primeiro número: "));
+const num2 = parseFloat(prompt("Digite o segundo número: "));
+
+alert("As operações básicas com " + num1 + " e " + num2 + " são:" +
+    "\nSOMA: " + (num1 + num2) +
+    "\nSUBTRAÇÃO: " + (num1 - num2) + 
+    "\nMULTIPLICAÇÃO: " + (num1 * num2) +
+    "\nDIVISÃO: " + (num1 / num2)
+)
