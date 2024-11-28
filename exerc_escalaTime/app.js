4a75194f9e335cb1032c4ddbdfcb7f2624d9f947
João Pedro Borba Pereira <joaopborba95@hotmail.com>
2024-11-27 22:32:58 -0300

    finalizado DOM

--- exerc_escalaTime/app.js
+++ exerc_escalaTime/app.js
@@ -1,21 +1,35 @@
 function escalar(){
-    const playersList = document.getElementById('playersList')
-
-    const ul = document.createElement("ul");
-    const newLi = document.createElement("li");
+    const playerName = document.getElementById('playerName').value
+    const playerNumber = document.getElementById('playerNumber').value
+    const position = document.getElementById('position').value
     
-    let confirmacao = confirm("Deseja salvar os dados desse jogador? \n " +
-        "Jogador: " + playerName.value + " - Posição: " + position.value + " - Número da camisa: " + playerNumber.value + "?"
+    const confirmacao = confirm("Deseja salvar os dados desse jogador? \n " +
+        "Jogador: " + playerName + " - Posição: " + position + " - Número da camisa: " + playerNumber + "?"
     )
     
     if(confirmacao){
-        newLi.innerText = "Jogador: " + playerName.value + " - Posição: " + position.value + " - Número da camisa: " + playerNumber.value;
-        playerName.value = ""
-        position.value = ""
-        playerNumber.value = ""
+        const playersList = document.getElementById('playersList')
+        const newLi = document.createElement('li')
+        newLi.id = 'player-' + playerNumber
+        newLi.innerText = "Jogador: " + playerName + " - Posição: " + position + " - Número da camisa: " + playerNumber;
+        playersList.appendChild(newLi)
+        document.getElementById('playerName').value = ""
+        document.getElementById('playerNumber').value = ""
+        document.getElementById('position').value = ""
     }
+    
 
-    ul.appendChild(newLi)
-    playersList.appendChild(ul)
+}
+
+function remover(){
+    const playerNumber = document.getElementById('numberToRemove').value
+    const playerToRemove = document.getElementById('player-' + playerNumber)
+
+    const confirmacao = confirm("Deseja remover o jogador com o número: " + playerNumber + "?")
+
+    if(confirmacao){
+        document.getElementById('playersList').removeChild(playerToRemove)
+        document.getElementById('numberToRemove').value = ""
+    }
 
 }
